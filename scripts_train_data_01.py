370a6a6076512c74d4338454712ae1ea71bea28d
adriben <benamira.adrien@gmail.com>
2020-03-18 11:25:48 +0800

    fix bug

--- scripts_train_data_01.py
+++ scripts_train_data_01.py
@@ -7,7 +7,7 @@ random.seed(0)
 #for n_pairs in ["little_dataset_100p/", "dataset_10000p/"]:
 n_pairs = "dataset_10000p/"
 
-for limit in ["3_5/"]:
+for limit in ["12_15/"]:
 
 
     seed = random.randint(0,2)
@@ -17,12 +17,15 @@ for limit in ["3_5/"]:
 
 
     os.system(
-        'python3 main.py --n_epochs 5 --device ' + str(1) + ' --seed ' + str(seed) + ' --initialisation predict_model --sparse No --sparseKL No --train_dir '+path1+
+        'python3 main.py --device ' + str(1) + ' --seed ' + str(seed) + ' --initialisation random --sparse No --sparseKL No --train_dir '+path1+
         ' --test_dir '+path2+' --val_dir ' +path3)
 
     print()
     print()
     print()
+
+
+
 
 
 
