7e91401db79da9f78ddf165736153e02d310d1cb
adriben <benamira.adrien@gmail.com>
2020-05-18 17:36:33 +0800

    fix bug

--- scripts_all.py
+++ scripts_all.py
@@ -14,7 +14,7 @@ path1 = "./data/dataset_10000p/10_40/test/dimacs"
 path2 = "./data/dataset_10000p/10_40/test/pickle"
 os.system('python3 generate_data.py --dimacs_dir '+path1+' --out_dir '+path2)
 
-os.system('python3 split.py')
+#os.system('python3 split.py')
 
 
 path1 = "./data/mini_GIFT/train/dimacs"
@@ -36,4 +36,4 @@ path3 = "./data/dataset_10000p/10_40/test/pickle"
 path4= "./data/mini_GIFT/train/pickle"
 path5= "./data/mini_GIFT/val/pickle"
 path6= "./data/mini_GIFT/test/pickle"
-os.system('python3 main.py --cuda 0 --train_dir '+path1+' --val_dir '+path2+' --test_dir '+ path3 +' & python3 main.py --cuda 1 --train_dir '+path4+' --val_dir '+path5+' --test_dir '+path6)
+os.system('python3 main.py --device 0 --train_dir '+path1+' --val_dir '+path2+' --test_dir '+ path3 +' & python3 main.py --device 1 --train_dir '+path4+' --val_dir '+path5+' --test_dir '+path6)
