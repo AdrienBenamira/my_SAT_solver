c0cd49478a527985f7efd037a41f0615df7d893d
adriben <benamira.adrien@gmail.com>
2020-05-18 11:34:24 +0800

    generate samples !!! v7

--- split.py
+++ split.py
@@ -1,8 +1,9 @@
 import os
 from tqdm import tqdm
 
+PATH = "./data/Mini_GIFT_poubelle" #"./data/mini_GIFT"
 #filenames = os.listdir("./data/Mini_GIFT_poubelle")
-filenames = os.listdir("./data/mini_GIFT")
+filenames = os.listdir(PATH)
 
 dico_name = {"5":{"UNSAT.DIMACS":[], "SAT.DIMACS":[]}, "6":{"UNSAT.DIMACS":[], "SAT.DIMACS":[]}, "7":{"UNSAT.DIMACS":[], "SAT.DIMACS":[]}}
 
@@ -22,12 +23,12 @@ for round in ["5", '6']:
 for filename in tqdm(filenames):
     liste_interet = filename.split('_')
     if liste_interet[0] == "7":
-        os.remove(filename)
+        os.remove(PATH + "/"+ filename)
         cpt +=1
-
-    if int(liste_interet[1])>dico_max[liste_interet[0]]:
-        os.remove(filename)
-        cpt +=1
+    else:
+        if int(liste_interet[1])>dico_max[liste_interet[0]]:
+            os.remove(PATH +"/"+ filename)
+            cpt +=1
 
 print(cpt)
 
