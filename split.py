c2512348654ac624ea1985e1d05d9af558efadfc
adriben <benamira.adrien@gmail.com>
2020-05-18 11:17:32 +0800

    generate samples !!! v6

--- split.py
+++ split.py
@@ -1,16 +1,33 @@
 import os
 from tqdm import tqdm
 
-filenames = os.listdir("./data/Mini_GIFT_poubelle")
+#filenames = os.listdir("./data/Mini_GIFT_poubelle")
+filenames = os.listdir("./data/mini_GIFT")
 
-dico_name = {"5":{"UNSAT.DIMACS":[], "SAT.DIMACS":[]}, "6":{"UNSAT.DIMACS":[], "SAT.DIMACS":[]}}
+dico_name = {"5":{"UNSAT.DIMACS":[], "SAT.DIMACS":[]}, "6":{"UNSAT.DIMACS":[], "SAT.DIMACS":[]}, "7":{"UNSAT.DIMACS":[], "SAT.DIMACS":[]}}
 
 for filename in tqdm(filenames):
     liste_interet = filename.split('_')
     dico_name[liste_interet[0]][liste_interet[-1]].append(int(liste_interet[1]))
 
+dico_max = {"5":0, "6":0}
+cpt = 0
+
 for round in ["5", '6']:
     lst1 = dico_name[round]["UNSAT.DIMACS"]
     lst2 = dico_name[round]["SAT.DIMACS"]
-    lst3 = [value for value in lst1 if value in lst2]
-    print(max(lst3), round)
+    lst3 = [max(lst1), max(lst2)]
+    dico_max[round] = max(lst3)
+
+for filename in tqdm(filenames):
+    liste_interet = filename.split('_')
+    if liste_interet[0] == "7":
+        os.remove(filename)
+        cpt +=1
+
+    if int(liste_interet[1])>dico_max[liste_interet[0]]:
+        os.remove(filename)
+        cpt +=1
+
+print(cpt)
+
